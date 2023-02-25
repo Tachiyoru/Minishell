a56ced0cf5b33f7822e51e334534a7725837f3f0
ajeanne <ajeanne@student.42.fr>
2023-02-25 13:31:23 +0100

    okok

--- srcs/3-Exec/builtins/cd.c
+++ srcs/3-Exec/builtins/cd.c
@@ -6,11 +6,35 @@
 /*   By: ajeanne <ajeanne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:21:32 by sleon             #+#    #+#             */
-/*   Updated: 2023/02/25 06:43:28 by ajeanne          ###   ########.fr       */
+/*   Updated: 2023/02/25 07:16:11 by ajeanne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	modify_pwd(void)
+{
+	t_env	*env;
+	t_env	*env_tmp;
+	
+	env = *get_env();
+	env_tmp = env;
+	while (env_tmp && ft_strcmp(env_tmp->key, "PWD"))
+		env_tmp = env_tmp->next;
+	while (env && ft_strcmp(env->key, "OLDPWD"))
+		env = env->next;
+	if (env && env->val)
+		free(env->val);
+	env->val = ft_strdup(env_tmp->val);
+	if (!env->val)
+		return (1);
+	if (env_tmp && env_tmp->val)
+		free(env_tmp->val);
+	env_tmp->val = getcwd(NULL, 0);
+	if (!env_tmp->val)
+		return (g_error = 127, msg("getcwd fail"), 1);
+	return (0);
+}
 
 /**
  * @brief behave like the cmd cd, changing the directory from wich we operate
@@ -28,5 +52,6 @@ int	b_in_cd(t_val *option)
 		perror(NULL);
 		g_error = 1;
 	}
+	modify_pwd();
 	return (1);
 }
