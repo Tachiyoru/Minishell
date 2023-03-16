2726b2a90bf621c5eeaeaa1bbb6af4b3e6901926
ajeanne <ajeanne@student.42.fr>
2023-03-16 19:23:54 +0100

    AddindUnsetChanges

--- srcs/3-Exec/builtins/unset.c
+++ srcs/3-Exec/builtins/unset.c
@@ -6,11 +6,39 @@
 /*   By: ajeanne <ajeanne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:21:16 by sleon             #+#    #+#             */
-/*   Updated: 2023/02/22 19:25:08 by ajeanne          ###   ########.fr       */
+/*   Updated: 2023/03/16 19:20:43 by ajeanne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	key_checker_unset(char *key)
+{
+	int	i;
+
+	i = 1;
+	if ((key[0] >= '0' && key[0] <= '9')
+		|| ((key[0] < 'A' || (key[0] > 'Z'
+					&& key[0] <= 'a') || key[0] >= 'z')
+			&& key[0] != '_'))
+		return (0);
+	while (key[i] && ((key[i] >= '0' && key[i] <= '9')
+			|| (key[i] >= 'A' && key[i] <= 'Z')
+			|| (key[i] >= 'a' && key[i] <= 'z')
+			|| key[i] == '_'))
+		i++;
+	if (!key[i])
+		return (1);
+	return (0);
+}
+
+void	error_unset(char *key_val)
+{
+	g_error = 1;
+	msg("unset: `");
+	msg(key_val);
+	msg("' : not a valid identifier\n");
+}
 
 void	free_u_env(t_env *data)
 {
@@ -50,8 +78,11 @@ int	unset_cmd(char *key_d)
 {
 	t_env	**env;
 
-	if (!key_d)
+	if (!key_checker_unset(key_d) || !key_d)
+	{
+		error_unset(key_d);
 		return (1);
+	}
 	env = get_env();
 	unset_rem(env, key_d);
 	return (1);
