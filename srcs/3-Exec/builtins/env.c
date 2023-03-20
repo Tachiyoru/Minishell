de8f09324e61c345cd034a26438e2c0d3cf59467
ajeanne <ajeanne@student.42.fr>
2023-03-20 15:21:36 +0100

    fixEnv

--- srcs/3-Exec/builtins/env.c
+++ srcs/3-Exec/builtins/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sleon <sleon@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ajeanne <ajeanne@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:21:24 by sleon             #+#    #+#             */
-/*   Updated: 2023/03/06 15:24:58 by sleon            ###   ########.fr       */
+/*   Updated: 2023/03/20 15:19:50 by ajeanne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,13 @@ int	env_cmd(int fd)
 	env = *get_env();
 	while (env)
 	{
-		ft_putstr_fd(fd, env->key);
-		write(fd, "=", 1);
-		ft_putstr_fd(fd, env->val);
-		write(fd, "\n", 1);
+		if (env->val)
+		{
+			ft_putstr_fd(fd, env->key);
+			write(fd, "=", 1);
+			ft_putstr_fd(fd, env->val);
+			write(fd, "\n", 1);
+		}
 		env = env->next;
 	}
 	return (g_error = 0, 1);
