c06d61cd870832cca793be6f0037e22509a53fa8
Shanley LEON <sleon@e1r10p1.clusters.42paris.fr>
2023-03-28 14:21:56 +0200

    fix

--- srcs/3-Exec/builtins/cd.c
+++ srcs/3-Exec/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: sleon <sleon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:21:32 by sleon             #+#    #+#             */
-/*   Updated: 2023/03/28 11:12:05 by sleon            ###   ########.fr       */
+/*   Updated: 2023/03/28 14:21:46 by sleon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	modify_pwd(void)
 		env_tmp = env_tmp->next;
 	while (env && ft_strcmp(env->key, "OLDPWD"))
 		env = env->next;
-	if (env && env->val && env_tmp->val)
+	if (env && env->val && env_tmp && env_tmp->val)
 	{
 		free(env->val);
 		env->val = ft_strdup(env_tmp->val);
@@ -33,8 +33,9 @@ int	modify_pwd(void)
 		free(env_tmp->val);
 		env_tmp->val = getcwd(NULL, 0);
 	}
-	else
-		return (g_error = 127, msg("getcwd fail\n"), 1);
+	if (env_tmp)
+		if (!env_tmp->val)
+			return (g_error = 127, msg("getcwd fail\n"), 1);
 	return (0);
 }
 
