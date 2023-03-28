ea1f7716d93ad67ac11c4ebfc08c3bb374a66972
Shanley LEON <sleon@e1r10p1.clusters.42paris.fr>
2023-03-28 10:54:13 +0200

    fork

--- srcs/3-Exec/exec.c
+++ srcs/3-Exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: sleon <sleon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 13:35:37 by sleon             #+#    #+#             */
-/*   Updated: 2023/03/28 10:49:33 by sleon            ###   ########.fr       */
+/*   Updated: 2023/03/28 10:54:08 by sleon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ void	exec_call(t_pipex *exec, t_pipex *start)
 	g_error = 0;
 	if (!exec->cmd->val)
 		return ;
-	pid = 0;
+	pid = fork();
 	if (pid == -1)
 		printf("Error fork on cmd = %s\n", exec->cmd->val);
 	else if (pid == 0)
