209ae519f5e7c2d6b1e87620ff8c0ad4a2d05fe2
Shanley LEON <sleon@e1r10p1.clusters.42paris.fr>
2023-03-28 11:36:29 +0200

    fix

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sleon <sleon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 16:24:33 by sleon             #+#    #+#             */
-/*   Updated: 2023/03/28 10:54:30 by sleon            ###   ########.fr       */
+/*   Updated: 2023/03/28 11:36:19 by sleon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int	main(int ac, char **av, char **envp)
 
 	(void)av;
 	(void)ac;
-	// if (envp[0] == NULL)
-	// 	return (g_error);
+	if (envp[0] == NULL)
+		return (g_error);
 	store_env(envp);
 	set_shlvl();
 	init_signal1();
